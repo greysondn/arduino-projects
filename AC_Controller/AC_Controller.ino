a4d4eca38797064fc4a688f548741140fbf69790
That Guy <greysondn@gmail.com>
2015-07-15 09:14:43 -0400

    Update code in AC_Controller

--- AC_Controller/AC_Controller.ino
+++ AC_Controller/AC_Controller.ino
@@ -5,10 +5,46 @@
 #include <IRremote.h>
 #include <dht.h>
 
+// Analog input pin for DHT11
 #define DHT11_PIN A0
+
+// codes for remote controller
+#define RM_POWER      0x8166817E
+#define RM_TEMP_DOWN  0x816651AE
+#define RM_TEMP_UP    0x8166A15E
+#define RM_FAN_SPEED  0x81669966
+#define RM_TIMER      0x8166F906
+#define RM_MODE       0x8166D926
+
+// whether or not the AC is on
+int ac_on = false;
+
+// temperature and humidity
+float temperature = -1.0;
+float humidity    = -1.0;
 
 IRsend irsend;
 dht DHT;
+
+/**
+ * Gets temp and humidity from DHT11 and stashes them in globals
+ */
+void getDHT()
+{
+      DHT.read11(DHT11_PIN);
+      humidity = DHT.humidity;
+      temperature = DHT.temperature;
+}
+
+/**
+ * fires a controller code for power
+ */
+void flipPower()
+{
+    irsend.sendNEC(RM_POWER, 32);
+    ac_on = !ac_on;
+}
+
 
 /**
  * Initial program setup.
@@ -16,7 +52,8 @@ dht DHT;
  */
 void setup()
 {
-    Serial.begin(9600);
+    // use this to show power status
+    pinMode(13, OUTPUT);
 }
 
 /**
@@ -25,41 +62,27 @@ void setup()
  */
 void loop()
 {
-    // ------------------------
-    // IR LED logic
-    // ------------------------
-    if (Serial.read() != -1)
+    // so... let's begin!
+    getDHT();
+    
+    // if it's too hot and the AC is off
+    if ((temperature > 25) && (!ac_on))
     {
-        // this is the timer button on my AC, which works as a good test
-        // that doesn't require any configuration changes to the AC itself.
-        irsend.sendNEC(0x8166F906, 32);
+          // turn the AC on!
+          flipPower();
+          
+          // turn LED on so we know the AC should be on
+          digitalWrite(13, HIGH);
     }
     
-    // -------------------------
-    // DHT 11 output
-    // -------------------------
-    int dhtStatus = DHT.read11(DHT11_PIN);
-    
-    if (dhtStatus == DHTLIB_OK)
+    // if it's too cold and the AC is on
+    if ((temperature < 21) && (ac_on))
     {
-        Serial.print("Humidity: ");
-        Serial.print(DHT.humidity, 2);
-        Serial.println("");
+        // turn the AC off!
+        flipPower();
         
-        Serial.print("Temp Cel: ");
-        Serial.print(DHT.temperature, 2);
-        Serial.println("");
-        
-        Serial.print("---");
-        Serial.println("");
-    }
-    else
-    {
-        Serial.print("Something's wrong.");
-        Serial.println("");
-        
-        Serial.print("---");
-        Serial.println("");
+        // turn LED on so we know the AC should be on
+        digitalWrite(13, LOW);
     }
     
     // we can only read the DHT so fast, so a delay here.
